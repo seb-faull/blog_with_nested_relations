3c758031973cc5a537385abd2efe0210df6926a1
seb-faull <seb_faull@hotmail.co.uk>
2017-10-04 18:55:42 +0100

    Added validations to both Comments and Posts models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
 	belongs_to :post
+
+	validates :comment, presence: :true
+
+	validates :comment, length: { minimum: 4, maximum: 1000, message: "You must enter a valid comment. Total characters - 1000" }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ApplicationRecord
 	has_many :comments
+
+	validates :title, :name, :body, presence: :true
+
+	validates :title, length: { maximum: 50, message: "Sorry, your title is too long. 50 characters allowed only" }
+
+	validates :name, length: { minimum: 4, maximum: 25, message: "Sorry, your name is too long. 25 characters allowed only" }
+
+	validates :body, length: { maximum: 5000, message: "Sorry, your Blog Post is too long. 5000 characters allowed only" }
+
+
+
+	# t.text "title"
+ #    t.text "name"
+ #    t.text "body"
+ #    t.datetime "created_at", null: false
+ #    t.datetime "updated_at", null: false
 end
