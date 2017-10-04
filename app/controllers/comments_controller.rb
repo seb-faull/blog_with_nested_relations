2a6d2047e5ac666dc5eb6f114470e40ada8a9793
seb-faull <seb_faull@hotmail.co.uk>
2017-10-04 17:10:56 +0100

    Added html to index and edit view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,64 @@
 class CommentsController < ApplicationController
-  
+
+
+  #Index
   def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
   end
 
+
+  #Show
   def show
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
+
+  #New
   def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
   end
 
+
+  #Create
   def create
+    post = Post.find(params[:post_id])
+    post.comments.create(comment_params)
+
+    redirect_to post
   end
 
+
+  #Edit
   def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
+
+  #Update
   def update
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    comment.update(comment_params)
+
+    redirect_to post_comment_path
   end
 
+
+  #Destroy
   def destroy
+    post = Post.find(params[:post_id])
+    post.comments.destroy(params[:id])
+
+    redirect_to post
   end
+
+  protected         
+  def comment_params
+    params.require(:comment).permit(:comment, :post_id)
+  end
+
 end
